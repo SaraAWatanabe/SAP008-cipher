31b7b15b0a91834b42c6e2883888752f9ab463b1
sara <saraakwatanabe@gmail.com>
2022-07-20 16:48:32 -0300

    primeiro commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,22 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, mensagem) {
+    
+  let cifragem = ((mensagem.charCodeAt(i) - 65 + offset) % 26 + 65) 
+   for (var i = 0; i<String.length; i++) // Como rodar todas as letras da mensagem?
+   
+  console.log (cifragem)
 
-export default cipher;
+  let mensagemCifrada = String.fromCharCode (cifragem)
+  console.log (mensagemCifrada)// o resultado é a mensagem criptografada
+  const output = document.getElementById("result")
+    output.write (mensagemCifrada) //Tá certo????
+  } ,
+  //decode: function 
+
+}
+
+
+
+//function decode(offset, string): offset
+
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import cipher from './cipher.js';
 
-console.log(cipher);
+import cipher from './cipher.js'  
+
+const mensagem = document.getElementById("mensagem")
+const offset = document.getElementById("offset")
+const btnEncode = document.getElementById("encode")
+const btnDecode = document.getElementById("decode")
+
+
+btnEncode.addEventListener ("click",function (){
+  console.log (mensagem.value)
+  console.log (offset.valueAsNumber)
+  //console.log(cipherEncode)
+  cipher.encode (offset.valueAsNumber, mensagem.value)
+})
+
+
+
+btnDecode.addEventListener ("click", function () {
+ 
+  
+  
+  })
+
+
